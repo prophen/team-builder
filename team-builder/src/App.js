5ec1431e6d1ed1c7bd166252deaeb7ff37330a28
Nikema Prophet <nikema@gmail.com>
2019-07-24 16:17:06 -0700

    Styling form

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 import MemberCard from "./components/MemberCard";
 import Form from "./components/Form";
 
--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -1,45 +1,66 @@
 import React, { useState } from "react";
-import {Button} from 'rebass'
+import { Button, Box, Flex } from "rebass";
 
 function Form() {
-  const [user, setUser] = useState({ username: "", password: "" });
+  const [member, setMember] = useState({
+    name: "",
+    email: "",
+    img: "",
+    role: ""
+  });
 
   const handleChange = event => {
-    setUser({ ...user, [event.target.name]: event.target.value });
+    setMember({ ...member, [event.target.name]: event.target.value });
   };
 
   const handleSubmit = event => {
     event.preventDefault();
-    console.log(user.name);
-    console.log(user.password);
+    console.log({ ...member });
   };
 
-
   return (
-    <div>
-      {console.log(user)}
+    <Box mx='auto'>
+      {console.log(member)}
       <form onSubmit={event => handleSubmit(event)}>
-        <label>
-          Username:
-          <input
-            type="text"
-            name="username"
-            value={user.username}
-            onChange={event => handleChange(event)}
-          />
-        </label>
-        <label>
-          Password:
-          <input
-            type="text"
-            name="password"
-            value={user.password}
-            onChange={event => handleChange(event)}
-          />
-        </label>
-        <Button>Submit!</Button>
+     
+        <Flex justifyContent={'center'}>
+        <Box width={'auto'} m='auto' >
+        <Flex flexDirection={"column"} alignItems={"flex-start"}>
+          <label>
+            Name: 
+            <input
+              type="text"
+              name="name"
+              value={member.name}
+              onChange={event => handleChange(event)}
+            />
+          </label>
+          <label>
+            Email: 
+            <input
+              type="text"
+              name="email"
+              value={member.email}
+              onChange={event => handleChange(event)}
+            />
+          </label>
+          <label>
+            Role: 
+            <input
+              type="text"
+              name="role"
+              value={member.role}
+              onChange={event => handleChange(event)}
+            />
+          </label>
+          </Flex>
+          </Box>
+          
+          </Flex>
+          <Button alignSelf={'center'}>Submit!</Button>
+        
       </form>
-    </div>
+    </Box>
   );
 }
 
